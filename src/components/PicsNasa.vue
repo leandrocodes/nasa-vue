2060444fa3fa9bbdf09e4615a612cfaceac93d92
leandrocodes <leaxviana140@gmail.com>
2019-09-02 18:36:40 -0400

    stylish

--- src/components/PicsNasa.vue
+++ src/components/PicsNasa.vue
@@ -1,9 +1,9 @@
 <template>
     <div>
-        <input type="text" v-model="query" :key-up="getPics()" />
+        <input type="text" v-model="query" @keyup.prevent="getPics()" />
         <p>{{ query }}</p>
 
-        <div v-if="results">
+        <div class="results" v-if="results != '' ">
             <div v-for="result in results" class="img-result" :key="result">
                 <img v-bind:src="result.links[0].href" />
                 <p>{{result.data[0].title.substring(0,80)}}</p>
@@ -38,8 +38,45 @@ export default {
 </script>
 
 <style>
-img {
+ input[type="text"] {
+    max-width: 960px;
+    width: 100%;
+    padding: 8px;
+    /*font-size: 16px;*/
+  }
+
+  .results {
+    max-width: 960px;
+    margin: 0 auto;
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+  }
+  .img-result {
+    position: relative;
+    margin: 1em;
+    width: 200px;
     height: 200px;
-    width: 250px;
-}
+    float: left;
+    display: flex;
+    align-items: flex-end;
+    justify-content: center;
+  }
+  .img-result p {
+    width: 100%;
+    z-index: 10;
+    padding: 8px;
+    margin: 0;
+    color: white;
+    text-align: center;
+    text-shadow: 0px 1px 3px rgba(0,0,0,0.4);
+    background: transparent;  /* fallback for old browsers */
+    background: linear-gradient(to bottom, rgba(0,0,0,0), rgba(0,0,0,0.5)); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+    
+  }
+  .img-result img {
+    position:absolute;
+    width: inherit; height: inherit;
+    object-fit: cover;
+  }
 </style>

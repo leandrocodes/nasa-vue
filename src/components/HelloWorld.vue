c21bc2703cf604fccfb08b5abd73f85b0dfb6213
leandrocodes <leaxviana140@gmail.com>
2019-09-02 17:53:52 -0400

    getting results

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,41 @@
 <template>
     <div>
-        <input type="text" v-model="query" v-on:key-up="getPics()">
+        <input type="text" v-model="query" :key-up="getPics()" />
         <p>{{ query }}</p>
+
+        <div v-for="result in results" class="img-result" :key="result">
+            <img v-bind:src="result.links[0].href" />
+            <p>{{result.data[0].title.substring(0,80)}}</p>
+        </div>
     </div>
 </template>
 
 <script>
-import axios from 'axios'
+import axios from "axios"
 export default {
     data() {
         return {
             query: "",
-            results: "",
+            results: ""
         }
     },
     methods: {
         getPics() {
-          axios.get(`https://images-api.nasa.gov/search?q=${this.query}&media_type=image`)
-              .then(res =>{
-                this.results = res.data.collection.items
-              })
-              .catch(err =>{
-                console.log(err)
-              })
+            axios
+                .get(
+                    `https://images-api.nasa.gov/search?q=${this.query}&media_type=image`
+                )
+                .then(res => {
+                    this.results = res.data.collection.items
+                })
+                .catch(err => {
+                    console.log(err)
+                })
         }
     }
 }
 </script>
 
 <style>
+
 </style>

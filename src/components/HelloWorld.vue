165f8acb14266ed03ae809e216cacfc6a0526b6f
leandrocodes <leaxviana140@gmail.com>
2019-09-02 17:36:59 -0400

    tryin to fetch data

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,13 +1,13 @@
 <template>
     <div>
-        <input type="text" v-model="query" />
+        <input type="text" v-model="query" v-on:key-up="getPics()">
         <p>{{ query }}</p>
     </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-    name: "HelloWolrd",
     data() {
         return {
             query: "",
@@ -16,7 +16,13 @@ export default {
     },
     methods: {
         getPics() {
-          this.results = 'result goes here...'
+          axios.get(`https://images-api.nasa.gov/search?q=${this.query}&media_type=image`)
+              .then(res =>{
+                this.results = res.data.collection.items
+              })
+              .catch(err =>{
+                console.log(err)
+              })
         }
     }
 }
